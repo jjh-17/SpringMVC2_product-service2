c31ea504749d8a4aa2e50129cb5ad23724a246fd
JJH <jakebbak@gmail.com>
2023-05-11 17:15:58 +0900

    2023/05/11: 타임리프_스프링 통합과 폼 - 배달 코드

---
+++ src/main/java/springMVC/productservice/domain/DeliveryCode.java
@@ -0,0 +1,12 @@
+package springMVC.productservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+//배송 코드: FAST, NORMAL, SLOW
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+    private String code; //시스템에서 전달하는 값
+    private String displayName; //고객에게 보여주는 정보
+}
