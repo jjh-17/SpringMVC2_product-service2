b59e7839bba47e63d1711408b468834344d1ec73
JJH <jakebbak@gmail.com>
2023-05-11 17:16:38 +0900

    2023/05/11: 타임리프_스프링 통합과 폼 - 상품 종류

---
+++ src/main/java/springMVC/productservice/domain/ProductType.java
@@ -0,0 +1,15 @@
+package springMVC.productservice.domain;
+
+import lombok.Getter;
+
+//상품 종류
+@Getter
+public enum ProductType {
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+    ProductType(String description) {
+        this.description = description;
+    }
+}
