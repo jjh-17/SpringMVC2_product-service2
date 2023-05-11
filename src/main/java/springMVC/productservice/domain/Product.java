9acd48e3ea07a6c0b0333ffe4ca6cc40b8655ccb
JJH <jakebbak@gmail.com>
2023-05-11 17:14:58 +0900

    2023/05/11: 상품 정보

--- src/main/java/springMVC/productservice/domain/Product.java
+++ src/main/java/springMVC/productservice/domain/Product.java
@@ -3,13 +3,24 @@ package springMVC.productservice.domain;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Product {
 
+    //기본 정보
     private Long id;
     private String name;
     private Integer price; //null이 할당될 수 있으므로 int 대신 Integer
     private Integer quantity;
+
+    //스프링 통합, 폼
+    private Boolean open; //판매 여부
+    private List<String> regions; //등록 지역
+    private ProductType productType; //상품 종류
+    private String deliveryCode; //배송 방식
+
+    //메시지, 국제화
 
     public Product() {
 
