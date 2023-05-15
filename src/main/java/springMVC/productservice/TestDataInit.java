b22aa391068fa9979c39ef87d38133929bae73f3
JJH <jakebbak@gmail.com>
2023-05-15 10:18:20 +0900

    2023/05/14: 빌드 파일

--- src/main/java/springMVC/productservice/TestDataInit.java
+++ src/main/java/springMVC/productservice/TestDataInit.java
@@ -4,17 +4,23 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import springMVC.productservice.domain.Product;
+import springMVC.productservice.domain.Product2;
 import springMVC.productservice.domain.ProductRepository;
+import springMVC.productservice.domain.ProductRepository2;
 
 @Component
 @RequiredArgsConstructor
 public class TestDataInit {
     private final ProductRepository productRepository;
+    private final ProductRepository2 productRepository2;
 
     //테스트용 데이터 추가
     @PostConstruct
     public void testInit() {
         productRepository.save(new Product("A", 10000, 10));
         productRepository.save(new Product("B", 20000, 20));
+
+        productRepository2.save(new Product2("A", 10000, 10));
+        productRepository2.save(new Product2("B", 20000, 20));
     }
 }
