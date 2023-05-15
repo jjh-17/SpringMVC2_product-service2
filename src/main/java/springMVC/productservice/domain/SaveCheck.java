a615dc1f4e6e5411d233facaf8d0aaf3af0f41c3
JJH <jakebbak@gmail.com>
2023-05-15 10:24:49 +0900

    2023/05/14: Bean Validation - group

---
+++ src/main/java/springMVC/productservice/domain/SaveCheck.java
@@ -0,0 +1,5 @@
+package springMVC.productservice.domain;
+
+//상품 저장용 group
+public interface SaveCheck {
+}
---
+++ src/main/java/springMVC/productservice/domain/UpdateCheck.java
@@ -0,0 +1,5 @@
+package springMVC.productservice.domain;
+
+//상품 수정용 group
+public interface UpdateCheck {
+}
