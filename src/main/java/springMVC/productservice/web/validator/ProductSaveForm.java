bf7cde48e5c6cf240773b1b8ab6733525cd5ee0f
JJH <jakebbak@gmail.com>
2023-05-15 10:36:00 +0900

    2023/05/14: Validation - Form 전송 객체 분리 - Form

---
+++ src/main/java/springMVC/productservice/web/validator/ProductSaveForm.java
@@ -0,0 +1,21 @@
+package springMVC.productservice.web.validator;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+public class ProductSaveForm {
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(9999)
+    private Integer quantity;
+}
---
+++ src/main/java/springMVC/productservice/web/validator/ProductUpdateForm.java
@@ -0,0 +1,23 @@
+package springMVC.productservice.web.validator;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+public class ProductUpdateForm {
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    //수정에서는 수량은 자유롭게 변경할 수 있다.
+    @NotNull
+    private Integer quantity;
+}
